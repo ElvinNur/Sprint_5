cdcd4eb2843c9f9c85002479cef4b6032b381293
ElvinNur <elvinnurmam@gmail.com>
2024-09-01 23:26:11 +0300

    Проверь переход по клику на «Конструктор» и на логотип Stellar Burgers.

--- tests/test_personal_account_to_constructor.py
+++ tests/test_personal_account_to_constructor.py
@@ -11,5 +11,9 @@ driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_m
 driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_main-default' and @name='Пароль']").send_keys("qwertyu")
 driver.find_element(By.XPATH, "//button[text()='Войти']").click()
 driver.find_element(By.XPATH, "(//p[@class='AppHeader_header__linkText__3q_va ml-2'])[3]").click()  # клик по кнопке "Личный кабинет"
-WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//p[@class='Account_text__fZAIn text text_type_main-default']"))) # проверка на наличие текста "В этом разделе вы можете изменить свои персональные данные"
+driver.find_element(By.XPATH, "//p[text()='Конструктор']").click() # клик по кнопке "Конструктор"
+WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//h1[text()='Соберите бургер']"))) # проверка на наличие текста "Соберите бургер"
+driver.find_element(By.XPATH, "(//p[@class='AppHeader_header__linkText__3q_va ml-2'])[3]").click() # клик по кнопке "Личный кабинет"
+driver.find_element(By.CSS_SELECTOR, "#root > div > header > nav > div > a > svg").click() # клик по лого
+WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//h1[text()='Соберите бургер']")))
 driver.quit()
